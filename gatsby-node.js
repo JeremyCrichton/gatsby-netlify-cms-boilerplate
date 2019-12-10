102519cc3d03d8987be48fc5b696c1d3c7df78ed
Jeremy Crichton <jeremy_crichton@hotmail.com>
2019-12-10 17:31:55 +0800

    revert to previous settings

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,40 @@
-const path = require(`path`)
+// const path = require(`path`)
 
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ actions, graphql, reporter }) => {
+//   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+//   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
 
-  const result = await graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
+//   const result = await graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  // Handle errors
-  if (result.errors) {
-    reporter.panicOnBuild(`Error while running GraphQL query.`)
-    return
-  }
+//   // Handle errors
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`)
+//     return
+//   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: `blog/${node.frontmatter.slug}`,
-      component: blogPostTemplate,
-      context: {
-        slug: `${node.frontmatter.slug}`,
-      }, // additional data can be passed via context
-    })
-  })
-}
+//   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: `/blog/${node.frontmatter.slug}`,
+//       component: blogPostTemplate,
+//       context: {
+//         slug: `${node.frontmatter.slug}`,
+//       }, // additional data can be passed via context
+//     })
+//   })
+// }
